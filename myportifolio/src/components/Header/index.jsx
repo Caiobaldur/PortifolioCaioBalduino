73505687ae0c23e88d152e267f86c8eba6ea1272
Caio Balduino <caiobrafachino@hotmail.com>
2023-10-13 19:38:35 -0300

    🔨chore: aligned and styled title and list.

--- myportifolio/src/components/Header/index.jsx
+++ myportifolio/src/components/Header/index.jsx
@@ -1,10 +1,23 @@
-import { Container } from "./styles";
+import { Container, Name, Link } from "./styles";
 
 export function Header() {
 
   return (
     <Container>
-      
+        
+          <Name>
+            <h1>Caio Balduino Rafachino</h1>
+          </Name>
+            <Link>
+              <ul>
+                <li>Home</li>
+                <li>Projetos</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+              </ul>
+            </Link>
+              
+  
     </Container>
   )
 }
--- myportifolio/src/components/Header/styles.js
+++ myportifolio/src/components/Header/styles.js
@@ -4,7 +4,29 @@ export const Container = styled.header`
   grid-area: header;
   height: 6.5rem;
   width: 100%;
-  border-bottom: .1rem;
+  border-bottom: 0.1rem;
   border-bottom-style: solid;
-  border-bottom-color: ${({theme}) => theme.COLORS.GREEN_200};
+  border-bottom-color: ${({ theme }) => theme.COLORS.GREEN_200};
+  font-family: var(--h-font);
+  color: #fff;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0 30rem;
+`
+
+export const Name = styled.div`
+  display: flex;
+`
+
+export const Link = styled.div`
+  > ul {
+    display: flex;
+    gap: 2rem;
+    list-style: none;
+    font-size: 1.6rem;
+  }
+
 `;
