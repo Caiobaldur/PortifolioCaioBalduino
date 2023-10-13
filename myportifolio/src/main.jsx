9b991e692a41cf932bbea8d4f5400c5932c21f92
Caio Balduino <caiobrafachino@hotmail.com>
2023-10-13 18:35:41 -0300

    💫feat: base components structure. (styled install)

--- myportifolio/src/main.jsx
+++ myportifolio/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import GlobalStyles from './styles/global.js'
 import {ThemeProvider} from 'styled-components'
 
+import { Home } from "./pages/Home"
+
 import theme from './styles/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-    <App />
+        <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
